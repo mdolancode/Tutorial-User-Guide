a9c4b352aed7903acda009e7cc6fc7342c62140e
Matthew Dolan <matthew@rocketplantech.com>
2023-05-05 06:15:32 -0700

    Two end points cannot be the same.

--- main.py
+++ main.py
@@ -2,21 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-
 # Cannot have two endpoints with the same path operation and parameters
-#@app.get("/users")
-#async def read_users():
+@app.get("/users")
+async def read_users():
     return ["Rick", "Morty"]
 
 
-#@app.get("/users")
-#async def read_users2():
-#    return ["Bean", "Elfo"]
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
