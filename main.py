4882b6503d6c18b93b5eda6a6f50a4ccbf4f89db
Matthew Dolan <matthew@rocketplantech.com>
2023-05-05 06:43:53 -0700

    Path convertor

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-from enum import Enum
-
 from fastapi import FastAPI
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
 
 app = FastAPI()
 
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
