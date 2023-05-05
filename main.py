d608d56428319b719595418bf8f9fc8e52532e54
Matthew Dolan <matthew@rocketplantech.com>
2023-05-05 06:14:50 -0700

    Two endpoints cannot be there same

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+# Cannot have two endpoints with the same path operation and parameters
+#@app.get("/users")
+#async def read_users():
+    return ["Rick", "Morty"]
+
+
+#@app.get("/users")
+#async def read_users2():
+#    return ["Bean", "Elfo"]
